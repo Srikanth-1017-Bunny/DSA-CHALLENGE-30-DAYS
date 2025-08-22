fd7d942b77595c17e8270a134fffaa11a6ff9d08
Khandesh-Srikanth <Khandeshsrikanth9@gmail.com>
2025-08-22 20:20:34 +0530

    Day-3 Solutions

---
+++ PalindromeNumber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int number=x;
+        int reverse=0;
+        while(x>0){
+           int lastDigit=x%10;
+            reverse=(reverse*10)+lastDigit;
+            x=x/10;
+        }
+
+        if(reverse==number){
+            return true;
+        }else{
+            return false;
+        }
+
+        
+    }
+}
