769bc2e0cd27ad217be4c383d458e69f7184fd6a
Khandesh-Srikanth <Khandeshsrikanth9@gmail.com>
2025-08-23 21:26:29 +0530

    Day-4 Solutions

---
+++ SingleNumber.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> hm=new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(hm.get(nums[i])==1){
+                return nums[i];
+            }
+        }
+
+        return -1;
+
+        
+    }
+}
