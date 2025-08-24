6070aaf19839744009f40093c20c7e3e7b7853ad
Khandesh-Srikanth <Khandeshsrikanth9@gmail.com>
2025-08-24 21:15:32 +0530

    Day-5 Solutions

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) { 
+        if(strs==null || strs.length==0){
+            return " ";
+        }  
+        if(strs.length==1){
+            return strs[0];
+        }
+        Arrays.sort(strs);
+
+        String str1=strs[0];
+        String str2=strs[strs.length-1];
+
+        int index=0;
+        while(index<str1.length() && index<str2.length()){
+            if(str1.charAt(index)==str2.charAt(index)){
+
+                index++;
+            }else{
+                break;
+            }
+        } 
+        return str1.substring(0,index);      
+    }
+}
