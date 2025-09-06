00d5c1f1d62e370f5d09e7592f226eada7ed8304
Khandesh-Srikanth <Khandeshsrikanth9@gmail.com>
2025-09-06 18:50:11 +0530

    Day-18 Solutions

---
+++ KthClosestPointsToOrigin.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> pq=new PriorityQueue<int[]>((a,b)->(b[0]*b[0]+b[1]*b[1])-a[0]*a[0]-a[1]*a[1]);
+
+
+        for(int[] pt:points){
+            pq.offer(pt);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        int[][] res=new int[pq.size()][2];
+        int count=0;
+        while(!pq.isEmpty()){
+            res[count]=pq.poll();
+            count++;
+        }
+        return res;
+    }
+}
